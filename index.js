7df6566fa94e2514dfef54daa08bb387939f9b14
alfia <4617alfia@gmail.com>
2023-05-27 15:25:49 +0700

    add login

--- index.js
+++ index.js
@@ -13,31 +13,70 @@ const db = admin.firestore();
 
 app.use(express.json());
 
-// API endpoint untuk user registration
+// API endpoint untuk user register
 app.post('/api/register', async (req, res) => {
     const { name, email, password } = req.body;
 
-    // Validasi panjang password
+    // validasi panjang password
     if (password.length < 8) {
         return res.status(400).json({ error: true, message: 'Password must be at least 8 characters' });
     }
 
     try {
-        // Cek apakah email sudah terdaftar
+        // cek apakah email sudah terdaftar
         const snapshot = await db.collection('users').where('email', '==', email).get();
         if (!snapshot.empty) {
             return res.status(400).json({ error: true, message: 'Email already exists' });
         }
 
-        // Simpan data user ke Firebase Firestore
+        // menyimpan data user ke firebase
         await db.collection('users').add({ name, email, password });
 
-        // Response sukses
+        // respon sukses
         res.status(201).json({ error: false, message: 'User Created' });
     } catch (error) {
-        // Jika terjadi kesalahan saat menyimpan data
+        // respon jika ada kesalahan data
         console.error('Error registering user:', error);
         res.status(500).json({ error: true, message: 'Failed to register user' });
+    }
+});
+
+// API endpoint untuk user login
+app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // mencari user berdasarkan email
+        const querySnapshot = await db.collection('users').where('email', '==', email).get();
+        if (querySnapshot.empty) {
+            return res.status(404).json({ error: true, message: 'User not found' });
+        }
+
+        // ambil data user dari hasil query
+        const user = querySnapshot.docs[0].data();
+
+        // verifikasi password
+        if (user.password !== password) {
+            return res.status(401).json({ error: true, message: 'Invalid password' });
+        }
+
+        // generate token JWT menggunakan firebase-adm
+        const customToken = await admin.auth().createCustomToken(user.userId);
+
+        // respon dengan data user dan token
+        res.status(200).json({
+            error: false,
+            message: 'Success',
+            loginResult: {
+                userId: user.userId,
+                name: user.name,
+                token: customToken,
+            },
+        });
+    } catch (error) {
+        // respon jika terjadi kesalahan saat akses database
+        console.error('Error logging in:', error);
+        res.status(500).json({ error: true, message: 'Failed to login' });
     }
 });
 
